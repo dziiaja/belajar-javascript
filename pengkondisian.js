ce8b73cce7e2d83f563b921b2a345708740f51ed
unknown <zikrigumilar2008@gmail.com>
2024-01-29 14:07:11 +0700

    Pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,9 @@
+var x = prompt("Masukan angka:");
+
+if (x % 2 == 0) {
+    alert(x + ' bilangan genap');
+} else if(x % 2 == 1) {
+    alert(x + ' bilangan ganjil');
+} else {
+    alert("Yang anda masukan salah");
+}
