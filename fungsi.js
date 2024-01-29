08960efc09345eb27b704bed1bd6d1f80710b8dc
unknown <zikrigumilar2008@gmail.com>
2024-01-29 13:03:48 +0700

    parameter fungsi javascript

---
+++ fungsi.js
@@ -0,0 +1,11 @@
+var x = "25";
+var y = "3.14";
+
+var toNum = parseInt(x); //fungsi dari parseint adalah mengubah string menjadi number
+var toFloat = parseFloat(y); //fungsi dari perseFloat adalah mengubah string menjadi desimal
+
+function jumlah(a, b) {
+    return a + b;
+}
+
+alert(jumlah(20, 10));
